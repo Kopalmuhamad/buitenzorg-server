ba72dcbadd4eb841b7bbf81a163cb01ed3126937
Kopalmuhamad <muhamadkopal32@gmail.com>
2024-10-24 19:39:19 +0700

    error in cors

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(ExpressMongoSanitize())
 app.use(bodyParser.json());
 app.use(express.static("public"))
 app.use(cors({
-    origin: 'http://localhost:5173/',
+    origin: 'http://localhost:3000/',
     credentials: true,
 }));
 app.use(cookieParser())
